31b072f2a2035cc677f1e4fdc58a97344b441ae7
jayaprakash <jayaprakashjp187@gmail.com>
2023-06-22 16:35:54 +0530

    [creating a simple list]

---
+++ LinkedListDS/LinkedList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListDS
+{
+    internal class LinkedList
+    {
+        private Node head;
+
+        public void Insert(int data)
+        {
+            Node newNode = new Node(data);
+
+            if (head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                Node current = head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = newNode;
+            }
+        }
+
+        public void Display()
+        {
+            Node current = head;
+            while (current != null)
+            {
+                Console.Write(current.Data + " ");
+                current = current.Next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ LinkedListDS/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LinkedListDS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("choose what program u want in a linked list");
+            Console.WriteLine("1.creating a simple linkedlist");
+            int choose =Convert.ToInt32(Console.ReadLine());
+            switch (choose)
+            {
+                case 1:
+                    LinkedList myList = new LinkedList();
+
+                    myList.Insert(56);
+                    myList.Insert(30);
+                    myList.Insert(70);
+
+                    myList.Display();
+                    break;
+
+            }
+
+        }
+    }
+}
